d99eb7e75787fbae7799009c4e15c97d1c7763fb
Felipe Tavares <felipetpadua26@gmail.com>
2025-05-22 15:45:27 -0300

    chore: maybe corrected stack auth changes

--- stack.tsx
+++ stack.tsx
@@ -1,11 +1,14 @@
 import "server-only";
 
-import { StackServerApp } from "@stackframe/stack";
+import {StackServerApp} from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
   urls: {
     signIn: "/signin",
     signUp: "/signup",
+    afterSignUp: "/",
+    afterSignIn: "/",
+    afterSignOut: "/",
   },
 });
